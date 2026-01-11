e81b78952df7932b293fcbc4a1098f467251917a
pipidaxian <78637637+pipidaxian@users.noreply.github.com>
2026-01-11 20:47:01 +0800

    Update header.php

--- layout/include/header.php
+++ layout/include/header.php
@@ -9,7 +9,7 @@ require_once 'dashboard/include/connection.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>كتب pdf</title>
+    <title>曲谱pdf</title>
     <!-- Bootstrab css-->
     <link rel="stylesheet" href="layout/css/bootstrap.min.css">
     <!-- Bootstrab RTL-->
@@ -22,7 +22,7 @@ require_once 'dashboard/include/connection.php';
     <!-- Start navbar-->
     <nav class="navbar navbar-expand-sm navbar-light">
         <div class="container">
-            <a href="index.php" class="navbar-brand">琴谱 pdf</a>
+            <a href="index.php" class="navbar-brand">曲谱pdf</a>
 
             <button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
                 <span class="navbar-toggler-icon"></span>
@@ -57,7 +57,7 @@ require_once 'dashboard/include/connection.php';
                         <div class="container" style="margin-top: 5px;">
                             <form action="search.php" method="GET">
                                 <div class="input-group">
-                                    <input class="form-control" type="text" name="search" placeholder="搜索书名或作者 " style="width: 300px;">
+                                    <input class="form-control" type="text" name="search" placeholder="搜索曲谱或作者 " style="width: 300px;">
                                     <div class="input-group-btn">
                                         <button class="btn btn-default" type="submit">
                                             <i class="glyphicon glyphicon-search" name="btn-submit">搜索 </i>
