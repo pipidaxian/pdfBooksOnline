74dd2800e9d440afb4bb146485fb3b2ca28ca0b5
pipidaxian <78637637+pipidaxian@users.noreply.github.com>
2026-01-11 19:14:53 +0800

    Update header.php

--- dashboard/include/header.php
+++ dashboard/include/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>لوحة التحكم</title>
+  <title>控制面板 </title>
   <!-- favicon -->
   <link rel="icon" type="image/png" href="images/book.png">
   <!-- Bootstrap and Bootstrap Rtl -->
@@ -21,16 +21,16 @@
 
     <!-- Sidebar -->
     <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">لوحة التحكم</div>
+      <div class="sidebar-heading">控制面板 </div>
       <div class="list-group list-group-flush">
-        <a href="dashboard.php" class="list-group-item list-group-item-action bg-light">نظرة عامة</a>
-        <a href="profile.php" class="list-group-item list-group-item-action bg-light">البروفايل</a>
-        <a href="categories.php" class="list-group-item list-group-item-action bg-light">التصنيفات</a>
+        <a href="dashboard.php" class="list-group-item list-group-item-action bg-light">概览 </a>
+        <a href="profile.php" class="list-group-item list-group-item-action bg-light">个人资料 </a>
+        <a href="categories.php" class="list-group-item list-group-item-action bg-light">分类 </a>
         <a href="#" class="list-group-item list-group-item-action bg-light" data-toggle="collapse" data-target="#menu">الكتب</a>
         <!-- Collapse menu -->
         <ul class="collapse sub-menu" id="menu">
-          <a href="new-book.php" class="list-group-item list-group-item-action bg-light">كتاب جديد</a>
-          <a href="books.php" class="list-group-item list-group-item-action bg-light">كل الكتب</a>
+          <a href="new-book.php" class="list-group-item list-group-item-action bg-light">新曲谱 </a>
+          <a href="books.php" class="list-group-item list-group-item-action bg-light">所有曲谱 </a>
         </ul>
       </div>
     </div>
@@ -47,7 +47,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="../index.php" target="_blank">عرض الموقع <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="../index.php" target="_blank">导航链接 <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -60,7 +60,7 @@
                 ?>
               </a>
              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="logout.php">تسجيل الخروج</a>
+                <a class="dropdown-item" href="logout.php">退出 </a>
               </div>
             </li>
           </ul>
